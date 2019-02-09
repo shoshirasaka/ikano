f9efdf941400bc90b570c8a8855e4a470c0c8ab0
白坂翔 <pizz@shirasakashou-no-MacBook-Air.local>
2019-02-09 22:31:29 +0900

    Added

---
+++ en/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+  <title>When is the best time for you? MAKER</title>
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1,user-scalable=no, minimal-ui">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"><meta name="msapplication-TileColor" content="#2d88ef">
+<meta name="msapplication-TileImage" content="favicons/mstile-144x144.png">
+<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
+<link rel="icon" type="image/vnd.microsoft.icon" href="../favicon.ico">
+<link rel="apple-touch-icon" sizes="57x57" href="../favicons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../favicons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../favicons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../favicons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../favicons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../favicons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../favicons/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../favicons/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../favicons/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="36x36" href="../favicons/android-chrome-36x36.png">
+<link rel="icon" type="image/png" sizes="48x48" href="../favicons/android-chrome-48x48.png">
+<link rel="icon" type="image/png" sizes="72x72" href="../favicons/android-chrome-72x72.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../favicons/android-chrome-96x96.png">
+<link rel="icon" type="image/png" sizes="128x128" href="../favicons/android-chrome-128x128.png">
+<link rel="icon" type="image/png" sizes="144x144" href="../favicons/android-chrome-144x144.png">
+<link rel="icon" type="image/png" sizes="152x152" href="../favicons/android-chrome-152x152.png">
+<link rel="icon" type="image/png" sizes="192x192" href="../favicons/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="256x256" href="../favicons/android-chrome-256x256.png">
+<link rel="icon" type="image/png" sizes="384x384" href="../favicons/android-chrome-384x384.png">
+<link rel="icon" type="image/png" sizes="512x512" href="../favicons/android-chrome-512x512.png">
+<link rel="icon" type="image/png" sizes="36x36" href="../favicons/icon-36x36.png">
+<link rel="icon" type="image/png" sizes="48x48" href="../favicons/icon-48x48.png">
+<link rel="icon" type="image/png" sizes="72x72" href="../favicons/icon-72x72.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../favicons/icon-96x96.png">
+<link rel="icon" type="image/png" sizes="128x128" href="../favicons/icon-128x128.png">
+<link rel="icon" type="image/png" sizes="144x144" href="../favicons/icon-144x144.png">
+<link rel="icon" type="image/png" sizes="152x152" href="../favicons/icon-152x152.png">
+<link rel="icon" type="image/png" sizes="160x160" href="../favicons/icon-160x160.png">
+<link rel="icon" type="image/png" sizes="192x192" href="../favicons/icon-192x192.png">
+<link rel="icon" type="image/png" sizes="196x196" href="../favicons/icon-196x196.png">
+<link rel="icon" type="image/png" sizes="256x256" href="../favicons/icon-256x256.png">
+<link rel="icon" type="image/png" sizes="384x384" href="../favicons/icon-384x384.png">
+<link rel="icon" type="image/png" sizes="512x512" href="../favicons/icon-512x512.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../favicons/icon-16x16.png">
+<link rel="icon" type="image/png" sizes="24x24" href="../favicons/icon-24x24.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../favicons/icon-32x32.png">
+<link rel="manifest" href="/manifest.json">
+<link rel="stylesheet" type="text/css" href="../jquery.datetimepicker.css"/ >
+<link rel="stylesheet" type="text/css" href="../sp.css"/ >
+  
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://cdn.jsdelivr.net/clipboard.js/1.5.3/clipboard.min.js"></script>
+<script src="../jquery.datetimepicker.js"></script> 
+</head>
+<body> 
+<div id="h1box">
+<h1>When is the best time for you? MAKER</h1></div>
+<input id="datetimepicker" type="text" >
+<div class="textarea_box">
+<textarea id="result">When is the best time for you?</textarea>
+  
+    <button class="btn copy_btn" data-clipboard-target="#result">COPY<span class="tooltip"><span class="text">Copied!</span></span></button>
+    <button class="btn reset_btn">RESET</button>
+</div>
+  
+ 
+<script> 
+$(function () {
+  var urlSearch = location.search;
+  urlSearch = urlSearch.slice(1);
+  urlSearch = decodeURI(urlSearch);
+  //console.log(urlSearch);  
+  if(urlSearch){
+    //$("#result").val(urlSearch);
+    //$("h1").html(urlSearch + "Maker");
+  }
+  
+
+var onSelectTimeFlag;
+
+  $('#datetimepicker').datetimepicker({
+	formatTime:'H:i',
+	formatDate:'d.m.Y',
+	defaultTime:'10:00',
+  inline: true,
+  step:30,
+  lang: 'en',
+	timepickerScrollbar:false,
+  onSelectDate:function(dateText){
+    var now_result = $("#result").val();
+    $("#result").val(now_result + "\n" + dateText.dateFormat('l, F j'));
+    //var WeekChars = [ "（日）", "（月）", "（火）", "（水）", "（木）", "（金）", "（土）" ];
+    //var wDay = dateText.getDay();
+    //var now_result = $("#result").val();
+    //$("#result").val(now_result + WeekChars[wDay]);
+      onSelectTimeFlag = false;
+  },
+  onSelectTime:function(dateText){
+    var now_result = $("#result").val();
+    if(onSelectTimeFlag){
+      $("#result").val(now_result + dateText.dateFormat('H:i)'));
+      onSelectTimeFlag = false;
+    }else{
+      $("#result").val(now_result + dateText.dateFormat(' (H:i-'));
+      onSelectTimeFlag = true;
+    }
+  },
+});
+  
+$('.copy_btn').click(function() {
+    var copyText = document.querySelector('#result');
+    var range = document.createRange();
+    range.selectNode(copyText);
+    getSelection().removeAllRanges();
+    getSelection().addRange(range);
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copy command was ' + msg);
+        //alert('コピーしました');
+        $(this).addClass("clicked");
+    } catch(err) {
+        console.log('Oops, unable to copy');
+    }
+    // 選択状態を解除する  
+    window.getSelection().removeAllRanges();
+});
+  
+  
+$('.reset_btn').click(function() {
+    $("#result").val("When is the best time for you?");
+    });
+});
+</script>
+  
+  
+
+<footer>© <?php date("Y");?> <a href="https://twitter.com/shoshirasaka/status/1093343767417958400" target="_blank">SHO SHIRASAKA</a>. </footer>  
+</body>
+</html>
